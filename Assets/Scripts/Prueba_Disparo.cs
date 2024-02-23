55edd9282372ce5cc74f6d66c77841f1ba2e0b3b
Rubiio14 <147033219+Rubiio14@users.noreply.github.com>
2024-02-23 03:02:11 +0000

    Juego_Naves_1.0.1
    Crosshair & ObjectPool

---
+++ Assets/Scripts/Prueba_Disparo.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prueba_Disparo : MonoBehaviour
+{
+    [SerializeField]
+    GameObject balaTipo1;
+    [SerializeField]
+    GameObject balaTipo2;
+   
+
+    int Speed = 130;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObjectPool.PreLoad(balaTipo1, 5);
+        ObjectPool.PreLoad(balaTipo2, 7);
+
+       
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //raycast
+        if (Physics.Raycast(transform.position, transform.TransformDirection(0, 0, 1), out RaycastHit hitinfo, 20f))
+        {
+            Debug.Log("Hit Something");
+            Debug.DrawRay(transform.position, transform.TransformDirection(0, 0, 1) * hitinfo.distance, Color.red);
+        }
+        else
+        {
+            Debug.Log("Hit Nothing");
+            Debug.DrawRay(transform.position, transform.TransformDirection(0, 0, 1) * 20f, Color.green);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            GameObject Bala1 = ObjectPool.GetObject(balaTipo1);
+
+
+
+            Rigidbody rb = balaTipo1.GetComponent<Rigidbody>();
+            //Add force to bullet
+            rb.AddForce(transform.TransformDirection(0, 0, 1) * Speed);
+            StartCoroutine(Recicle(balaTipo1, Bala1, 2.0f));
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            GameObject Bala2 = ObjectPool.GetObject(balaTipo2);
+
+            Rigidbody rb_2 = balaTipo2.GetComponent<Rigidbody>();
+
+            //Add force to bullet
+            rb_2.AddForce(transform.TransformDirection(0, 0, 1) * Speed);
+            StartCoroutine(Recicle(balaTipo2, Bala2, 2.0f));
+        }
+    }
+    
+    IEnumerator Recicle(GameObject primitive, GameObject go, float time)
+    {
+        yield return new WaitForSeconds(time);
+        ObjectPool.RecicleObject(primitive, go);
+    }
+}
